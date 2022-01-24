d79665bead9d912ee8d6363ea27940fe2b865bc4
DeshmukhShilpa <58388877+DeshmukhShilpa@users.noreply.github.com>
2022-01-24 19:09:20 +0530

    Initial Commit

---
+++ RatingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RatingApp
+//
+//  Created by Shilpa on 24/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
